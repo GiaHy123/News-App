0b5a1abfee952cd76727cfedc3c3974a5f5dd12e
GiaHy123 <honggiahy@gmail.com>
2022-10-20 10:48:04 +0700

    Appbar Search

--- news_app/lib/screens/search/search.dart
+++ news_app/lib/screens/search/search.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: prefer_const_constructors, duplicate_ignore
+
+
 import 'package:flutter/material.dart';
 
 class Search extends StatefulWidget {
@@ -10,11 +13,90 @@ class Search extends StatefulWidget {
 class _SearchState extends State<Search> {
   @override
   Widget build(BuildContext context) {
+    // ignore: prefer_const_constructors
     return Scaffold(
       appBar: AppBar(
         title: const Text("Search"),
+        // ignore: avoid_unnecessary_containers, sized_box_for_whitespace
+        // ignore: prefer_const_literals_to_create_immutables
+        actions: [
+          IconButton(
+            onPressed: (){
+              showSearch(
+                context: context,
+                delegate: CustomSearch()
+                );
+            }, 
+            icon: const Icon(Icons.search)
+            )
+        ],
+          
       ),
-      body: Center(child: Text("Search screen")),
-    );;
+      body: Container(child: Text("Search screen")),
+    );
   }
+}
+
+class CustomSearch extends SearchDelegate{
+  List<String>allData = [
+    'Kuma','Russia','Winter','Hot'
+  ];
+
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return[
+      IconButton(
+        onPressed: (){
+          query = ' ';
+        }, 
+        icon: Icon(Icons.clear)
+        )
+    ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return(
+      IconButton(
+        onPressed: (){
+          close(context, null);
+        }, 
+        icon: Icon(Icons.arrow_back)
+        )
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    List<String> matchQuery = [];
+    for(var item in allData){
+      if(item.toLowerCase().contains(query.toLowerCase())){
+        matchQuery.add(item);
+      }
+    }
+    return ListView.builder(
+      itemCount: matchQuery.length,
+      itemBuilder: (context, index) {
+        var result = matchQuery[index];
+        return ListTile(title: Text(result));
+      });
+      
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    List<String> matchQuery = [];
+    for(var item in allData){
+      if(item.toLowerCase().contains(query.toLowerCase())){
+        matchQuery.add(item);
+      }
+    }
+    return ListView.builder(
+      itemCount: matchQuery.length,
+      itemBuilder: (context, index) {
+        var result = matchQuery[index];
+        return ListTile(title: Text(result));
+      });
+  }
+
 }
